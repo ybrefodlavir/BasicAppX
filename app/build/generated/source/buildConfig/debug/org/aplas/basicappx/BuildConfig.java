d16efc9fa631ddf8978b685b3345b700d1d113e5
ArinKistia22 <arinkistian22@gmail.com>
2021-09-14 01:13:02 +0700

    pertemuan 1

---
+++ app/build/generated/source/buildConfig/debug/org/aplas/basicappx/BuildConfig.java
@@ -0,0 +1,16 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package org.aplas.basicappx;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "org.aplas.basicappx";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Field from default config.
+  public static final int MIN_SDK_VERSION = 21;
+  // Field from default config.
+  public static final int TARGET_SDK_VERSION = 28;
+}

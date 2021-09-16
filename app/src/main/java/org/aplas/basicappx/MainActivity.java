41318cfb31a4e865b7acbc3be60a36c4486f9e22
ybrefodlavir <rivaldoferby69@gmail.com>
2021-09-16 11:24:07 +0700

    B1 Task 3

--- app/src/main/java/org/aplas/basicappx/MainActivity.java
+++ app/src/main/java/org/aplas/basicappx/MainActivity.java
@@ -3,12 +3,35 @@ package org.aplas.basicappx;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+    private Distance dist = new Distance();
+    private Weight weight = new Weight();
+    private Temperature temp = new Temperature();
+    private Button convertBtn;
+    private EditText inputTxt,outputTxt;
+    private Spinner unitOri,unitConv;
+    private RadioGroup unitType;
+    private CheckBox roundBox,formBox;
+    private ImageView imgView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    protected double convertUnit(String ){
+
+    }
+
+    protected double strResult(){
+
     }
 }
